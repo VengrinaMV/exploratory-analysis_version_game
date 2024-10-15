88971df2e015f7956990a21b292a3fea86ffd7de
VengrinaMV <vengrinamv@mail.ru>
2024-10-15 16:43:19 +0400

    eda.sql

--- eda.sql
+++ eda.sql
@@ -25,86 +25,231 @@ select max(price::int),
        percentile_disc(0.99) within group (order by price::int) as q99
  from products
 
- 
---запрос 6 - определение arpu
-select l.version
-	,count(distinct l.user_id) as count_user
-    ,avg(p.value) as avg_price
-	,sum(p.value ) as sum_price
-	,sum(p.value )/ count(distinct l.user_id) as ARPU
-from registrations  as r
-join logins   as l on r.user_id =l.user_id 
-left join purchases as p on r.user_id =p.user_id 
-join products  as p2 on p.product_id=p2.product_id 
-where r.player_install_source = 'organic' and (l.version='1.2' or l.version='1.1')
-group by  l.version
+--запрос 6 - определение arpdau
+select round(sum(rev) / (
+                    select count(distinct tt2.user_id) as users
+                    from game.logins as tt2
+                        join(
+                             select user_id
+                             from game.registrations
+                             where player_install_source = 'organic'
+                            ) as tt1 on tt1.user_id = tt2.user_id 
+                    where version in ('1.1')
+                   ), 2) as ARPDAU
+    ,  version
+from(
+    select sum(value) as rev
+        ,  t2.user_id
+        ,  version
+    from game.purchases as t2
+        join(
+                select min(time) as min_time
+                    ,  max(time) as max_time
+                    ,  t1.user_id 
+                    ,  version
+                from game.logins as t1
+                    join(
+                         select user_id
+                         from game.registrations
+                         where player_install_source = 'organic'
+                        ) as t4 on t1.user_id = t4.user_id 
+                where version in ('1.1')
+                group by version, t1.user_id
+                ) as t1 on t1.user_id = t2.user_id
+    where time between min_time and max_time
+    group by t2.user_id, version
+    ) as t3
+group by version
+union all 
+select round(sum(rev) / (
+                    select count(distinct tt2.user_id) as users
+                    from game.logins as tt2
+                        join(
+                             select user_id
+                             from game.registrations
+                             where player_install_source = 'organic'
+                            ) as tt1 on tt1.user_id = tt2.user_id 
+                    where version in ('1.2')
+                   ), 2) as ARPDAU
+    ,  version
+from(
+    select sum(value) as rev
+        ,  t2.user_id
+        ,  version
+    from game.purchases as t2
+        join(
+                select min(time) as min_time
+                    ,  max(time) as max_time
+                    ,  t1.user_id 
+                    ,  version
+                from game.logins as t1
+                    join(
+                         select user_id
+                         from game.registrations
+                         where player_install_source = 'organic'
+                        ) as t4 on t1.user_id = t4.user_id 
+                where version in ('1.2')
+                group by version, t1.user_id
+            ) as t1 on t1.user_id = t2.user_id
+    where time between min_time and max_time
+    group by t2.user_id, version
+    ) as t3
+group by version
 
---запрос 7 --сравление PU
-WITH us_by_group AS (   
-    SELECT l.version,
-           l.user_id,
-           COUNT(p.value) AS purchase_count
-    FROM registrations AS r
-    JOIN logins AS l ON r.user_id = l.user_id 
-    LEFT JOIN purchases AS p ON r.user_id = p.user_id 
-    WHERE r.player_install_source = 'organic' 
-      AND (l.version = '1.2' OR l.version = '1.1')
-    GROUP BY l.version, l.user_id
-),
-t1 AS (
-    SELECT version,
-           COUNT(DISTINCT user_id) AS us_all
-    FROM us_by_group
-    GROUP BY version
-),
-t2 AS (
-    SELECT version,
-           COUNT(DISTINCT user_id) AS us_paid
-    FROM us_by_group
-    WHERE purchase_count > 0
-    GROUP BY version
-),
-t3 AS (
-    SELECT t1.version,
-           us_all,
-           COALESCE(t2.us_paid, 0) AS us_paid
-    FROM t1
-    LEFT JOIN t2 ON t1.version = t2.version
-)
-SELECT version,
-       us_paid * 100.0 / NULLIF(us_all, 0) AS PU
-FROM t3; 
+--запрос 7 --сравнение PU
+select count(distinct user_id)::DOUBLE precision / 
+                         (
+                        select count(distinct tt2.user_id)
+                        from game.logins as tt2
+                            join(
+                                 select distinct user_id
+                                 from game.registrations
+                                 where player_install_source = 'organic'
+                                ) as tt1 on tt1.user_id = tt2.user_id 
+                        where version in ('1.1')
+                       )*100 as pu                      
+        ,  version
+    from(
+        select distinct t2.user_id
+            ,  version
+        from game.purchases as t2
+            join(
+                    select min(time) as min_time
+                        ,  max(time) as max_time
+                        ,  t1.user_id 
+                        ,  version
+                    from game.logins as t1
+                        join(
+                             select user_id
+                             from game.registrations
+                             where player_install_source = 'organic'
+                            ) as t4 on t1.user_id = t4.user_id 
+                    where version in ('1.1')
+                    group by version, t1.user_id
+                ) as t1 on t1.user_id = t2.user_id
+        where time between min_time and max_time
+        group by t2.user_id, version
+        ) as t3
+group by version 
+union all 
+select count(distinct user_id)::DOUBLE precision / 
+                         (
+                        select count(distinct tt2.user_id)
+                        from game.logins as tt2
+                            join(
+                                 select distinct user_id
+                                 from game.registrations
+                                 where player_install_source = 'organic'
+                                ) as tt1 on tt1.user_id = tt2.user_id 
+                        where version in ('1.2')
+                       )*100 as pu                  
+        ,  version
+    from(
+        select distinct t2.user_id
+            ,  version
+        from game.purchases as t2
+            join(
+                    select min(time) as min_time
+                        ,  max(time) as max_time
+                        ,  t1.user_id 
+                        ,  version
+                    from game.logins as t1
+                        join(
+                             select user_id
+                             from game.registrations
+                             where player_install_source = 'organic'
+                            ) as t4 on t1.user_id = t4.user_id 
+                    where version in ('1.2')
+                    group by version, t1.user_id
+        
+                ) as t1 on t1.user_id = t2.user_id
+        
+        where time between min_time and max_time
+        group by t2.user_id, version
+        ) as t3
+group by version
+
 
 --запрос 8
-WITH category_revenue AS (
-    SELECT t3.version,
-        category,
-        SUM(t1.value) AS category_total
-    FROM 
-        purchases t1
-    JOIN 
-        products t2 ON t1.product_id = t2.product_id 
-    JOIN 
-        logins t3 ON t1.user_id = t3.user_id 
-    WHERE 
-        t3.version IN ('1.1', '1.2')
-    GROUP BY 
-        category,t3.version
-),
-total_revenue AS (
-    SELECT 
-        SUM(category_total) AS total
-    FROM 
-        category_revenue
-)
-SELECT 
-	cr.version,
-    cr.category,
-    cr.category_total,
-    (cr.category_total / tr.total) * 100 AS revenue_share
-FROM 
-    category_revenue cr,
-    total_revenue tr
-ORDER BY 
-    2;
+with cte_table as(
+select sum(rev) as revenue
+    ,  count(*) as cnt
+    ,  version
+    ,  category
+from(
+    select sum(value) as rev
+        ,  t2.user_id
+        ,  product_id 
+        ,  version
+    from game.purchases as t2
+        join(
+                select min(time) as min_time
+                    ,  max(time) as max_time
+                    ,  t1.user_id 
+                    ,  version
+                from game.logins as t1
+                    join(
+                         select user_id
+                         from game.registrations
+                         where player_install_source = 'organic'
+                        ) as t4 on t1.user_id = t4.user_id 
+                where version in ('1.2')
+                group by version, t1.user_id
+            ) as t1 on t1.user_id = t2.user_id  
+    where time between min_time and max_time
+    group by t2.user_id, version, product_id
+    ) as t7 join(
+            select product_id
+                ,  category
+            from game.products
+            ) as t6 on t7.product_id = t6.product_id
+group by version, category
+union all 
+select sum(rev) as revenue
+    ,  count(*) as cnt
+    ,  version
+    ,  category
+from(
+    select sum(value) as rev
+        ,  t2.user_id
+        ,  product_id 
+        ,  version
+    from game.purchases as t2
+        join(
+                select min(time) as min_time
+                    ,  max(time) as max_time
+                    ,  t1.user_id 
+                    ,  version
+                from game.logins as t1
+                    join(
+                         select user_id
+                         from game.registrations
+                         where player_install_source = 'organic'
+                        ) as t4 on t1.user_id = t4.user_id 
+                where version in ('1.1')
+                group by version, t1.user_id
+            ) as t1 on t1.user_id = t2.user_id
+        where time between min_time and max_time
+    group by t2.user_id, version, product_id
+    ) as t7 join(
+            select product_id
+                ,  category
+            from game.products
+            ) as t6 on t7.product_id = t6.product_id
+group by version, category)
+select round(revenue / sum_rev, 2) as rev_share
+    ,  round(cnt / sum_cnt, 2) as orders_count_share
+    ,  ct.version 
+    ,  category
+from cte_table as ct
+    join(
+         select sum(revenue) as sum_rev    
+             ,  sum(cnt) as sum_cnt
+             ,  version
+         from cte_table
+         group by version
+        ) as ct2 on ct.version = ct2.version
+        order by 4 
+
+   
 
